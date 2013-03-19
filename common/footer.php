2414fabc9d3cc5c0a480f4b9c09977148cb88087
John Flatness <john@zerocrates.org>
2013-03-19 11:14:05 -0400

    Fix double nav wrapper in footer.
    Also remove ignored parameter.

--- common/footer.php
+++ common/footer.php
@@ -8,12 +8,8 @@
             <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
                 <p><?php echo $copyright; ?></p>
             <?php endif; ?>
-	    </div>
-		<ul class="navigation">
-			<?php echo public_nav_main(array(__('Home') => url(''), __('Browse Items') => url('items'), __('Browse Collections') => url('collections')));
-        	?>
-		</ul>
-
+        </div>
+        <nav><?php echo public_nav_main();?></nav>
         <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
 		
 	</div><!-- end footer-content -->
