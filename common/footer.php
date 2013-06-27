c6dba148283d559729562742f2d9c1c652cb89e3
Kim Nguyen <design@kimisgold.com>
2013-06-27 10:26:46 -0400

    Check for footer text to avoid empty divs.

--- common/footer.php
+++ common/footer.php
@@ -3,12 +3,14 @@
 <div id="footer">
 
     <div id="footer-content" class="center-div">
+        <?php if($footerText = get_theme_option('Footer Text')): ?>
         <div id="custom-footer-text">
             <p><?php echo get_theme_option('Footer Text'); ?></p>
-            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-                <p><?php echo $copyright; ?></p>
-            <?php endif; ?>
         </div>
+        <?php endif; ?>
+        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+        <p><?php echo $copyright; ?></p>
+        <?php endif; ?>
         <nav><?php echo public_nav_main()->setMaxDepth(0); ?></nav>
         <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
 
