4909d0cd12f8f9eaebfe03859c96f31db4cc94ed
Kim Nguyen <design@kimisgold.com>
2013-07-08 15:22:00 -0400

    Update html and title tags.

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php echo get_html_lang(); ?>">
+<html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
@@ -7,7 +7,13 @@
     <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
 
-    <title><?php echo option('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
 
     <?php echo auto_discovery_link_tags(); ?>
 
