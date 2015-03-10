479d5834c35da0ba792b3f1f804a45e551af8c8d
Kim Nguyen <design@kimisgold.com>
2015-03-10 13:34:16 -0400

    Update normalize, remove unnecessary modernizr js call, add collapse/expand arrow to top level links with sub navs.

--- common/header.php
+++ common/header.php
@@ -25,7 +25,6 @@
     echo head_css();
     ?>
     <!-- JavaScripts -->
-    <?php queue_js_file('vendor/modernizr'); ?>
     <?php queue_js_file('vendor/selectivizr', 'javascripts', array('conditional' => '(gte IE 6)&(lte IE 8)')); ?>
     <?php queue_js_file('vendor/respond'); ?>
     <?php queue_js_file('vendor/jquery-accessibleMegaMenu'); ?>
