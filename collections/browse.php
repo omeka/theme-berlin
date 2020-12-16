208099b1098b037c0d56368c2314d61780c6215b
kalbers <ken.albers@gmail.com>
2020-12-16 06:44:23 -0500

    Update for rich titles

--- collections/browse.php
+++ collections/browse.php
@@ -41,7 +41,7 @@ $sortLinks[__('Date Added')] = 'added';
     </div>
     <?php endif; ?>
 
-    <p class="view-items-link"><?php echo link_to_items_browse(__('View the items in %s', metadata('collection', array('Dublin Core', 'Title'))), array('collection' => metadata('collection', 'id'))); ?></p>
+    <p class="view-items-link"><?php echo link_to_items_browse(__('View the items in %s', metadata('collection', 'rich_title', array('no_escape' => true))), array('collection' => metadata('collection', 'id'))); ?></p>
 
     <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
 
