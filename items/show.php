f943f72265d6bc5e3144f259d52a8bb41778b369
Kim Nguyen <design@kimisgold.com>
2026-03-12 16:23:57 -0400

    Update item pagination markup.
    (omeka/plugin-FilePagination#4)

--- items/show.php
+++ items/show.php
@@ -40,11 +40,12 @@
     </div>
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
-
-    <ul class="item-pagination navigation">
-        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
+    <nav aria-label="<?php echo __('Item pagination'); ?>">
+        <ul class="item-pagination navigation">
+            <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+            <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+        </ul>
+    </nav>
 
 </div> <!-- End of Primary. -->
 
